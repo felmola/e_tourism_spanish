33e7487d22e9a9bf1441d2d290e5e14e858c5d32
Felipe Montealegre <43189041+felmola@users.noreply.github.com>
2019-12-17 01:38:20 -0500

    Deleted bot testing from apps

--- settings.py
+++ settings.py
@@ -44,17 +44,7 @@ SESSION_CONFIGS = [
         num_demo_participants=20,
         app_sequence=['app_0_consent', 'app_5_market_regret', 'app_8_summary', 'app_7_question', 'app_9_report'],
         use_browser_bots=False,
-    ),
-    dict(
-        name='bot_testing',
-        display_name="Testing bots",
-        num_demo_participants=20,
-        app_sequence=['app_0_consent', 'app_1_market_com_practices', 'app_8_summary', 'app_9_report'],
-#        app_sequence=['app_7_question'],
-        use_browser_bots=False, ),
-
-
-
+    )
 ]
 
 
